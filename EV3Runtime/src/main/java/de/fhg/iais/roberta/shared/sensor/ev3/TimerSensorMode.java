ce419bb71c9886e791481f9eeca2ed2172cd4cd3
Kostadin Cvejoski <cvejoski@gmail.com>
2016-05-03 15:07:49 +0200

    #2 Initial restructuring of EV3 Lejos artifacts

---
+++ EV3Runtime/src/main/java/de/fhg/iais/roberta/shared/sensor/ev3/TimerSensorMode.java
@@ -0,0 +1,8 @@
+package de.fhg.iais.roberta.shared.sensor.ev3;
+
+/**
+ * Mode of the time sensor.
+ */
+public enum TimerSensorMode {
+    RESET, GET_SAMPLE;
+}
---
+++ EV3Runtime/userProjects/60dkvjgn/src/NEPOprog.java
@@ -0,0 +1,71 @@
+package generated.main;
+
+import de.fhg.iais.roberta.runtime.*;
+import de.fhg.iais.roberta.runtime.ev3.*;
+
+import de.fhg.iais.roberta.shared.*;
+import de.fhg.iais.roberta.shared.action.ev3.*;
+import de.fhg.iais.roberta.shared.sensor.ev3.*;
+
+import de.fhg.iais.roberta.components.*;
+import de.fhg.iais.roberta.components.ev3.*;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import lejos.remote.nxt.NXTConnection;
+
+public class NEPOprog {
+    private static Ev3Configuration brickConfiguration;
+
+    private Set<UsedSensor> usedSensors = new LinkedHashSet<UsedSensor>();
+
+    private Hal hal = new Hal(brickConfiguration, usedSensors);
+
+    public static void main(String[] args) {
+        try {
+             brickConfiguration = new Ev3Configuration.Builder()
+                .setWheelDiameter(5.6)
+                .setTrackWidth(12.0)
+                .addActor(ActorPort.B, new EV3Actor(EV3Actors.EV3_LARGE_MOTOR, true, DriveDirection.FOREWARD, MotorSide.RIGHT))
+                .addActor(ActorPort.C, new EV3Actor(EV3Actors.EV3_LARGE_MOTOR, true, DriveDirection.FOREWARD, MotorSide.LEFT))
+                .addSensor(SensorPort.S1, new EV3Sensor(EV3Sensors.EV3_TOUCH_SENSOR))
+                .addSensor(SensorPort.S4, new EV3Sensor(EV3Sensors.EV3_ULTRASONIC_SENSOR))
+                .build();
+            new NEPOprog().run();
+        } catch ( Exception e ) {
+            lejos.hardware.lcd.TextLCD lcd = lejos.hardware.ev3.LocalEV3.get().getTextLCD();
+            lcd.clear();
+            lcd.drawString("Error in the EV3", 0, 0);
+            if (e.getMessage() != null) {
+                lcd.drawString("Error message:", 0, 2);
+                Hal.formatInfoMessage(e.getMessage(), lcd);
+            }
+            lcd.drawString("Press any key", 0, 7);
+            lejos.hardware.Button.waitForAnyPress();
+        }
+    }
+
+
+    public void run() throws Exception {
+
+        hal.turnOnRegulatedMotor(ActorPort.B, 10);
+        hal.waitFor(1000);
+        hal.drawText(String.valueOf(hal.getRegulatedMotorSpeed(ActorPort.B)), 0, 0);
+        hal.waitFor(1000);
+        hal.stopRegulatedMotor(ActorPort.B, MotorStopMode.FLOAT);
+        hal.setRegulatedMotorSpeed(ActorPort.B, 30);
+        hal.waitFor(1000);
+        hal.drawText(String.valueOf(hal.getRegulatedMotorSpeed(ActorPort.B)), 0, 2);
+        while ( true ) {
+            if ( hal.isPressed(BrickKey.ENTER) == true ) {
+                break;
+            }
+            hal.waitFor(15);
+        }
+
+    }
+}
